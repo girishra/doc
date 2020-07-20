ee00ed2f1f253287130f4e91366fe57b2d660715
Lincoln Kushwah <lincolnkushwah009@gmail.com>
2020-07-20 20:41:46 +0530

    code

--- lib/Screens/specialist.dart
+++ lib/Screens/specialist.dart
@@ -26,42 +26,46 @@ class _HomePageState extends State<Specialist> {
       body: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemCount: 20,
-
         itemBuilder: (BuildContext context, int index){
           print("categoryyy");
           print(categoryList);
 
-          return GestureDetector(
-            // onTap: (){
-            //   Navigator.of(context).pushNamed(PlayScreen.routeName);
-            // },
-            child: Container(
-              margin: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(color: Colors.transparent)),
-              child: Card(
-                margin: EdgeInsets.all(0),
-                // color: Color.fromRGBO(31, 0, 65, 100),
-                elevation: 8,
-                child: Column(children: <Widget>[
-                  SizedBox(
-                    height: 30,
-                  ),
-                 Image.network('https://www.sermo.com/wp-content/uploads/2017/07/shutterstock_448894387_v2.jpg'),
-                  
-                  
-                   Text(
-                      "Cardiology",
-                      style: TextStyle(color: Colors.black, fontSize: 20),
+          return Stack(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    elevation: 5,
+                    child: Image.network('https://www.sermo.com/wp-content/uploads/2017/07/shutterstock_448894387_v2.jpg'
+                    , fit:BoxFit.fill
+                        ,
+                      height: 180,
                     ),
-                  
-                ]),
-              ),
-            ),
+                  ),
+                ),
+                Positioned(
+                    bottom: 12,
+                    left: 12,
+                    child: ClipRRect(
+                    borderRadius: new BorderRadius.only(
+          topRight: const Radius.circular(20.0),
+          ),
+                      child: Container(
+                        height: 40,
+                        width: 120,
+                        color: Color.fromRGBO(105, 105 , 105, 50),
+                          child: Center(child: Text("someText",style: TextStyle(color: Colors.white),))),
+                    )),
+              ]
           );
         } ,
       ),
     );
   }
 }
+
+
+
+
+
+
